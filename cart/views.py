b33e3bab75d04061c3c5fd4ee350aecb313fa896
unknown <thejasmeet.singh88629@gmail.com>
2019-07-27 21:21:15 +0530

    Added Cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Cart
 
-# Create your views here.
+
+def cart_home(request):
+    cart_obj, new_obj = Cart.objects.new_or_get(request)  # get a existing cart or create a new one
+    return render(request, 'cart/home.html', cart_obj)
